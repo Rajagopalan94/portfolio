78cf5ebafe60a5a734a98db7cf6d8597152b67ca
Rajagopalan94 <rajagopalan@integrass.com>
2021-02-09 13:42:28 +0530

    adding a nav bar

---
+++ nav bar/nav.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+div {
+  padding: 20px;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+a:hover {
+  background-color: green;
+}
+
+.navbar {
+  overflow: hidden;
+  background-color: #FFFFFF;
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+.navbar a {
+  float: left;
+  font-size: 20px;
+  color: black;
+  text-align:center ;
+  padding: 20px 20px;
+  text-decoration: none;
+}
+
+.topnav-centered a {
+  float: none;
+  position: absolute;
+  top: 60%;
+  left: 30%;
+  transform: translate(-50%, -50%);
+}
+.topnav-centered a {
+    position: relative;
+    top: 0;
+    left: 1;
+    transform: none;
+  }
+}
+</style>
+</head>
+<body>
+<div class="navbar">
+<div class="topnav-centered">
+ <a href="#news">News</a>
+  <a href="#shop">Shop</a>
+  <a href="#contact">Contact</a>
+  <a href="#about-us">About-us</a>
+  <a href="#portfolio">Portfolio</a>
+  <a href="#about-us">Services</a>
+  <a href="#home">Home</a>
+ 
+  </div> 
+</div>
+
+</body>
+</html>
